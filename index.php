e49414cc8cbcac7e349c81a7fe72c24dc3cb4c33
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-07-03 12:55:52 -0300

    Atualizando

--- index.php
+++ index.php
@@ -10,7 +10,69 @@
 	<body>
 
 		<div class="content-main">
-			
+
+			<h1>Melhores Filmes</h1>
+
+			<div class="content">
+				<div class="filme-content">
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
+					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
+					<span class="filme-nota">10/10</span>
+					<span class="filme-ano">1969</span>
+				</div>
+				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
+			</div>
+
+			<div class="content">
+				<div class="filme-content">
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
+					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
+					<span class="filme-nota">10/10</span>
+					<span class="filme-ano">1969</span>
+				</div>
+				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
+			</div>
+
+			<div class="content">
+				<div class="filme-content">
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
+					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
+					<span class="filme-nota">10/10</span>
+					<span class="filme-ano">1969</span>
+				</div>
+				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
+			</div>
+
+			<div class="content">
+				<div class="filme-content">
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
+					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
+					<span class="filme-nota">10/10</span>
+					<span class="filme-ano">1969</span>
+				</div>
+				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
+			</div>
+
+			<div class="content">
+				<div class="filme-content">
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
+					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
+					<span class="filme-nota">10/10</span>
+					<span class="filme-ano">1969</span>
+				</div>
+				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
+			</div>
+
+			<div class="content">
+				<div class="filme-content">
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
+					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
+					<span class="filme-nota">10/10</span>
+					<span class="filme-ano">1969</span>
+				</div>
+				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
+			</div>
+
 			<div class="content">
 				<div class="filme-content">
 					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
