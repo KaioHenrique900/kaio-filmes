91b5b1236dadfc70b1ae93ab1d29e298e391ed7f
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-07-04 08:53:20 -0300

    Atualizando

--- index.php
+++ index.php
@@ -25,80 +25,80 @@
 
 			<div class="content">
 				<div class="filme-content">
-					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
-					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
-					<span class="filme-nota">10/10</span>
-					<span class="filme-ano">1969</span>
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/cidade-de-deus.jpg"></a>
+					<div class="filme-nome">Cidade de Deus</div>
+					<span class="filme-nota">9/10</span>
+					<span class="filme-ano">2002</span>
 				</div>
 				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
 			</div>
 
 			<div class="content">
 				<div class="filme-content">
-					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
-					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/clube-da-luta.jpg"></a>
+					<div class="filme-nome">Clube da Luta</div>
 					<span class="filme-nota">10/10</span>
-					<span class="filme-ano">1969</span>
+					<span class="filme-ano">1999</span>
 				</div>
 				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
 			</div>
 
 			<div class="content">
 				<div class="filme-content">
-					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
-					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
-					<span class="filme-nota">10/10</span>
-					<span class="filme-ano">1969</span>
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/django-livre.jpg"></a>
+					<div class="filme-nome">Django Livre</div>
+					<span class="filme-nota">9/10</span>
+					<span class="filme-ano">2012</span>
 				</div>
 				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
 			</div>
 
 			<div class="content">
 				<div class="filme-content">
-					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
-					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
-					<span class="filme-nota">10/10</span>
-					<span class="filme-ano">1969</span>
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/drive.jpg"></a>
+					<div class="filme-nome">Drive</div>
+					<span class="filme-nota">8.5/10</span>
+					<span class="filme-ano">2011</span>
 				</div>
 				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
 			</div>
 
 			<div class="content">
 				<div class="filme-content">
-					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
-					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
-					<span class="filme-nota">10/10</span>
-					<span class="filme-ano">1969</span>
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/era-uma-vez-em-hollywood.jpg"></a>
+					<div class="filme-nome">Era Uma Vez Em Hollywood...</div>
+					<span class="filme-nota">9/10</span>
+					<span class="filme-ano">2019</span>
 				</div>
 				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
 			</div>
 
 			<div class="content">
 				<div class="filme-content">
-					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
-					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
-					<span class="filme-nota">10/10</span>
-					<span class="filme-ano">1969</span>
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/faca-a-coisa-certa.jpg"></a>
+					<div class="filme-nome">Faça A Coisa Certa</div>
+					<span class="filme-nota">9.5/10</span>
+					<span class="filme-ano">1990</span>
 				</div>
 				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
 			</div>
 
 			<div class="content">
 				<div class="filme-content">
-					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
-					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/meupai.jpg"></a>
+					<div class="filme-nome">Meu Pai</div>
 					<span class="filme-nota">10/10</span>
-					<span class="filme-ano">1969</span>
+					<span class="filme-ano">2020</span>
 				</div>
 				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
 			</div>
 
 			<div class="content">
 				<div class="filme-content">
-					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
-					<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
-					<span class="filme-nota">10/10</span>
-					<span class="filme-ano">1969</span>
+					<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL"><img class="filme-image" src="images/o-grande-lebowski.jpg"></a>
+					<div class="filme-nome">o Grande Lebowski</div>
+					<span class="filme-nota">9/10</span>
+					<span class="filme-ano">2001</span>
 				</div>
 				<div class="filme-more"><div class="material-symbols-outlined">expand_more</div></div>
 			</div>
