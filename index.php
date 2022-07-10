8d104fb9e2db17dcf3792c0f7a02a29cb0e888b5
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-07-10 09:50:29 -0300

    Atualizando

--- index.php
+++ index.php
@@ -8,6 +8,16 @@
 	</head>
 
 	<body>
+
+		<div class="topnav" id="myTopnav">
+		  <a href="#home" class="active">Home</a>
+		  <a href="#news">News</a>
+		  <a href="#contact">Contact</a>
+		  <a href="#about">About</a>
+		  <a href="javascript:void(0);" class="icon" onclick="responsiveTopNav()">
+		    <i class="fa fa-bars"></i>
+		  </a>
+		</div>
 		
 		<div class="content-main">
 
