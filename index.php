96582a223763f8267a86e7a0acc08fffbd5721f2
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-10-05 17:27:22 -0300

    Updating

--- index.php
+++ index.php
@@ -108,10 +108,6 @@
 			  setTimeout(showSlides, 5000); // Change image every 2 seconds
 			}
 		</script>
-
-		<?php
-			print_r($filmes[0]);
-		?>
 		
 		<div class="content-main">
 			<center>
@@ -131,10 +127,10 @@
 			<div class="row">
 				<div class="content">
 					<div class="filme-content">
-						<a href="https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL" target="_blank"><img class="filme-image" src="images/2001-uma-odisseia-no-espaco.jpg"></a>
-						<div class="filme-nome">2001: Uma Odisséia No Espaço</div>
-						<span class="filme-nota">10/10</span>
-						<span class="filme-ano">1969</span>
+						<a href="<?php print_r($filmes[0]['linkFilme']); ?>" target="_blank"><img class="filme-image" src="images/<?php print_r($filmes[0]['capaFilme']); ?>"></a>
+						<div class="filme-nome"><?php print_r($filmes[0]['tituloFilme']); ?></div>
+						<span class="filme-nota"><?php print_r($filmes[0]['notaFilme']); ?>/10</span>
+						<span class="filme-ano"><?php print_r($filmes[0]['anoFilme']); ?></span>
 					</div>
 					<div class="filme-more" onclick="showModal('https://lafobo1015.cincopa.com/watch/A4HAcLOLOO68!AEFDgmUFT-oL', '2001: Uma Odisséia No Espaço', '10', '1969', '2h19','images/2001-uma-odisseia-no-espaco-horizontal.jpg', 'Stanley Kubrick', 'Uma estrutura imponente preta fornece uma conexão entre o passado e o futuro nesta adaptação enigmática de um conto reverenciado de ficção científica do autor Arthur C. Clarke. Quando o Dr. Dave Bowman e outros astronautas são enviados para uma misteriosa missão, os chips de seus computadores começam a mostrar um comportamento estranho, levando a um tenso confronto entre homem e máquina que resulta em uma viagem alucinante no espaço e no tempo.')"><div class="material-symbols-outlined">expand_more</div></div>
 				</div>
