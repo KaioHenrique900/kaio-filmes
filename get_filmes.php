64be7dc5f509e62d87fab8baf03a636d69882fe6
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-10-06 18:43:56 -0300

    Updating

--- get_filmes.php
+++ get_filmes.php
@@ -1,9 +1,18 @@
 <?php
 
-include 'bd_connect.php';
+//include 'bd_connect.php';
 
-public function getMelhores(){
-	$query = pg_query($this->$conn, 'SELECT * FROM filmes as f inner join filmes_categorias as fc on f."idFilme" = fc."fk_idFilme" and fc."fk_idCat" = 14');
+function getMelhores(){
+	$host="ec2-3-219-229-143.compute-1.amazonaws.com";
+	$port="5432";
+	$user="lffjupovuwkowk";
+	$database="d9ubq55up557s";
+	$password="9d5ed4768450704643a229dc1dc5329bd6a7b339eedffd464875a9398b678666";
+
+	$conn = pg_connect("host = ec2-3-219-229-143.compute-1.amazonaws.com port = 5432 dbname = d9ubq55up557s user = lffjupovuwkowk password = 9d5ed4768450704643a229dc1dc5329bd6a7b339eedffd464875a9398b678666 sslmode=require") or
+	die ("Não foi possível conectar ao servidor PostGreSQL");
+
+	$query = pg_query($conn, 'SELECT * FROM filmes as f inner join filmes_categorias as fc on f."idFilme" = fc."fk_idFilme" and fc."fk_idCat" = 14');
 	$filmes = Array();
 
 	if(pg_num_rows($query) > 0){
@@ -35,8 +44,7 @@ public function getMelhores(){
 		}
 	}
 	return $filmes;
+	pg_close($conn);
 }
 
-
-pg_close($conn);
 ?>
