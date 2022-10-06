deec13339cb2db1756041b577b5fd6119e97ec4f
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-10-06 18:36:54 -0300

    Updating

--- bd_connect.php
+++ bd_connect.php
@@ -8,7 +8,7 @@ $user="lffjupovuwkowk";
 $database="d9ubq55up557s";
 $password="9d5ed4768450704643a229dc1dc5329bd6a7b339eedffd464875a9398b678666";
 
-public $conn = pg_connect("host = ec2-3-219-229-143.compute-1.amazonaws.com port = 5432 dbname = d9ubq55up557s user = lffjupovuwkowk password = 9d5ed4768450704643a229dc1dc5329bd6a7b339eedffd464875a9398b678666 sslmode=require") or
+$conn = pg_connect("host = ec2-3-219-229-143.compute-1.amazonaws.com port = 5432 dbname = d9ubq55up557s user = lffjupovuwkowk password = 9d5ed4768450704643a229dc1dc5329bd6a7b339eedffd464875a9398b678666 sslmode=require") or
 die ("Não foi possível conectar ao servidor PostGreSQL");
 
 ?>
