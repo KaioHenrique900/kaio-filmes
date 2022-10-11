cfb2821901b125cbefc19de44f18f9a5fbd92adb
KaioHenrique900 <kaioneziohenrique@gmail.com>
2022-10-10 21:37:51 -0300

    Updating

--- index.php
+++ index.php
@@ -135,7 +135,7 @@
 						$titulo = $filme['tituloFilme'];
 						$nota = $filme['notaFilme'];
 						$ano = $filme['anoFilme'];
-						$duracao = $filme['duracaoFilme']
+						$duracao = $filme['duracaoFilme'];
 						$sinopse = $filme['sinopseFilme'];
 						echo "<div class='content'><div class='filme-content'><a href='".$link."' target='_blank'><img class='filme-image' src='images/".$capa."'></a><div class='filme-nome'>".$titulo."</div><span class='filme-nota'>".$nota."/10</span><span class='filme-ano'>".$ano."</span></div><div class='filme-more' onclick='showModal(".$link.", ".$titulo.", ".$nota.", ".$ano.", ".$duracao.", \'images/\'".$capa.", '\direcao\', ".$sinopse.")'><div class='material-symbols-outlined'>expand_more</div></div></div>";
 						
